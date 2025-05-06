554477a3c0ef24502db1045ea146a19a9652dfd3
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-05-06 17:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const form = document.querySelector('form');
+    const fname = form.elements['fname'].value;
+    const lname = form.elements['lname'].value;
+	alert(`${fname} ${lname}`);
 }
